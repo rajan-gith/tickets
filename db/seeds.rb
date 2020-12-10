f1243978694464291990164e6dc731283dc50845
rajan-gith <rajan.beryls@gmail.com>
2020-12-10 13:25:12 +0530

    devise file added and seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Role.where(name: 'admin').first_or_create
+Role.where(name: 'user').first_or_create
+User.admins.where(email: 'admin@gmail.com').first_or_create(password: 123456)
+User.users.where(email: 'user@gmail.com').first_or_create(password: 123456)
