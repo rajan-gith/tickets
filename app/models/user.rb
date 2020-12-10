986b18640c226b62f30f4eeaf77d2ce402de09a3
rajan-gith <rajan.beryls@gmail.com>
2020-12-10 13:24:33 +0530

    devise view and migration added fo users

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  belongs_to :role
+
+  scope :admins, -> () { where(role: Role.find_by(name: 'admin')) }
+  scope :users, -> () { where(role: Role.find_by(name: 'user')) }
+end
