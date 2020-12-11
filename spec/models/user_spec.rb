9dbb158e3a3e53356b9657b36247e264d7e5ff38
rajan-gith <rajan.beryls@gmail.com>
2020-12-11 20:02:34 +0530

    unit testing added

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  context 'roles validation' do
+    it 'ensure role should exist' do
+      user = User.new(email: "abc@gmail.com", password: 123456).save
+      expect(user).to eq(false)
+    end
+    it 'ensure admin role should exist' do
+      Role.where(name: 'admin').first_or_create
+      Role.where(name: 'user').first_or_create
+      user = User.new(email: "abc@gmail.com", password: 123456, role: Role.find_by(name: 'admin')).save
+      expect(user).to eq(true)
+    end
+
+    it 'ensure user role should exist' do
+      Role.where(name: 'admin').first_or_create
+      Role.where(name: 'user').first_or_create
+      user = User.new(email: "abc@gmail.com", password: 123456, role: Role.find_by(name: 'user')).save
+      expect(user).to eq(true)
+    end
+  end
+end
