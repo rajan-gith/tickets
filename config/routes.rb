99327927309f02a382b042aae921da45d2c9977d
rajan-gith <rajan.beryls@gmail.com>
2020-12-11 15:36:48 +0530

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
 
   get 'tickets/book/:id' , to: 'tickets#book', as: 'book_ticket'
   post 'tickets/book/:id' , to: 'tickets#create', as: 'create_ticket_booking'
+  get 'tickets/receipt/:id' , to: 'tickets#receipt', as: 'ticket_shows'
+  get 'tickets' , to: 'tickets#index', as: 'tickets_list'
 end
