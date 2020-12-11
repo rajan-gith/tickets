94e695c425b380cbee8b12d64fe3a232598fd1aa
rajan-gith <rajan.beryls@gmail.com>
2020-12-11 20:24:14 +0530

    models tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   context 'roles validation' do
     it 'ensure role should exist' do
       user = User.new(email: "abc@gmail.com", password: 123456).save
